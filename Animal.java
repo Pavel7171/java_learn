2b458046bbf7b376b7304324b9c023562f42fdb7
Pavel7171 <106702029+Pavel7171@users.noreply.github.com>
2022-08-08 16:46:03 +0300

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,10 @@
+public class Animal {
+    String nameDog = "Bob";
+
+    public void eat(){
+        System.out.println("i'm eating");
+    }
+    public void sleep(){
+        System.out.println("i'm sleeping");
+    }
+}
