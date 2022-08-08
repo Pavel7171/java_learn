41fd2b05e3efe19d4f71bc226555e3d2507c4e40
Pavel7171 <106702029+Pavel7171@users.noreply.github.com>
2022-08-08 10:13:27 +0300

    Create If.java

---
+++ If.java
@@ -0,0 +1,11 @@
+public class If {
+      public static void main(String [] args) {
+      int myInt = 14;
+      if (myInt>6) {
+      System.out.println("Yes");
+     }
+     else {
+   System.out.println("No");
+      }
+    }
+}
