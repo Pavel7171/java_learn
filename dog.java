8a01ddfaeedccff34d7b38d855e50f4e5c02b238
Pavel7171 <106702029+Pavel7171@users.noreply.github.com>
2022-08-08 16:46:19 +0300

    Create dog.java

---
+++ dog.java
@@ -0,0 +1,11 @@
+public class dog extends Animal {
+    public void bark(){
+        System.out.println("i'm barking");
+    }
+    public void eat(){
+        System.out.println("Dog is eating");
+    }
+    public void showName(){
+        System.out.println(nameDog);
+    }
+}
