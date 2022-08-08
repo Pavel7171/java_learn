be1151a242248c980443432bde455cf00071e05d
Pavel7171 <106702029+Pavel7171@users.noreply.github.com>
2022-08-08 10:16:18 +0300

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,19 @@
+ public class Variables{
+    public static void main(String[] args){
+        int myInt;
+        myInt = 123;
+        System.out.println(myInt);
+       // int -2147483648 - 2147483647
+        //short myShort = 3266; -32768 - 32767
+       // long myLong = 246445484; 
+
+       // double myDouble = 246.44;
+      //  float = myFloat = 454.4f;
+
+      //  char c = 'a'; symbols
+      //  boolean b = true; true or false
+
+      //  byte myByte = 100;  -128 - 127 
+        
+    }
+}
