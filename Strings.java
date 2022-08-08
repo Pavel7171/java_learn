7dca6dc9fc2a1d567aeadcd26428a0666de93fec
Pavel7171 <106702029+Pavel7171@users.noreply.github.com>
2022-08-08 10:15:42 +0300

    Create Strings.java

---
+++ Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+    public static void main(String[] args){
+        int x = 5;
+        String s = "Hello";
+        String space = " ";
+        String name = "bob";
+        System.out.println(s+space+name);
+        System.out.println("Hello"+" "+"Pavel");
+        System.out.println("My number is "+ x);
+    }
+}
