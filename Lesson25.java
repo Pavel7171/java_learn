1436eb99d61abeacf74b519533702400f90dbc9e
Pavel7171 <106702029+Pavel7171@users.noreply.github.com>
2022-08-08 16:45:43 +0300

    Create Lesson25.java

---
+++ Lesson25.java
@@ -0,0 +1,12 @@
+public class Lesson25 { //sm animal.java & dog.java
+    public static void main(String[] args) {
+       // Animal animal = new Animal();
+       // animal.eat();
+       // animal.sleep();
+        dog dog= new dog();
+        dog.sleep();
+        dog.eat();
+        dog.bark();
+        dog.showName();
+    }
+}
